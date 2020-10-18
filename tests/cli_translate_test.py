6ec05aa351ebbd243a136b7c4ff3a4092dc44d88
Chris Mackey <chris@ladybug.tools>
2020-10-18 01:02:34 -0400

    style(cli): Improve CLI commands
    This commit includes several fixes to the CLI commands that ensure the help docs display correctly and that simulations are run with similar conventions to honeybee-energy.

--- tests/cli_translate_test.py
+++ tests/cli_translate_test.py
@@ -1,5 +1,6 @@
 """Test cli translate module."""
 from click.testing import CliRunner
+from honeybee_energy.config import folders
 from dragonfly_energy.cli.translate import model_to_idf
 
 import os
@@ -9,9 +10,10 @@ def test_model_to_idf():
     runner = CliRunner()
     input_df_model = './tests/json/model_complete_simple.json'
 
-    result = runner.invoke(model_to_idf, [input_df_model])
+    output_df_folder = './tests/json/'
+    output_df_model = os.path.join(output_df_folder, 'OfficeBuilding.idf')
+    result = runner.invoke(model_to_idf, [input_df_model, '-f', output_df_folder])
     assert result.exit_code == 0
 
-    output_df_model = './tests/json/OfficeBuilding.idf'
     assert os.path.isfile(output_df_model)
     os.remove(output_df_model)
