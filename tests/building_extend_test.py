e62ea3561617167bc4a7e79e611752ac4024fae7
Chris Mackey <chris@ladybug.tools>
2021-01-22 19:38:04 -0500

    fix(building): Add a method to diversify programs
    I am also updating the CI and adding a Dockerfile so that we get a dragonfly-energy docker image with each release.

--- tests/building_extend_test.py
+++ tests/building_extend_test.py
@@ -151,6 +151,40 @@ def test_set_all_room_2d_hvac():
                for room in building.unique_room_2ds)
 
 
+def test_diversify():
+    """Test the diversify method."""
+    pts_1 = (Point3D(0, 0, 3), Point3D(0, 10, 3), Point3D(10, 10, 3), Point3D(10, 0, 3))
+    pts_2 = (Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(20, 10, 3), Point3D(20, 0, 3))
+    pts_3 = (Point3D(0, 10, 3), Point3D(0, 20, 3), Point3D(10, 20, 3), Point3D(10, 10, 3))
+    pts_4 = (Point3D(10, 10, 3), Point3D(10, 20, 3), Point3D(20, 20, 3), Point3D(20, 10, 3))
+    room2d_1 = Room2D('Office1', Face3D(pts_1), 3)
+    room2d_2 = Room2D('Office2', Face3D(pts_2), 3)
+    room2d_3 = Room2D('Office3', Face3D(pts_3), 3)
+    room2d_4 = Room2D('Office4', Face3D(pts_4), 3)
+    story = Story('OfficeFloor', [room2d_1, room2d_2, room2d_3, room2d_4])
+    story.solve_room_2d_adjacency(0.01)
+    story.set_outdoor_window_parameters(SimpleWindowRatio(0.4))
+    story.multiplier = 4
+    building = Building('OfficeBuilding', [story])
+
+    lab_program = office_program.duplicate()
+    lab_program.identifier = 'Bio Laboratory'
+    lab_program.electric_equipment.watts_per_area = 50
+    lab_program.ventilation.flow_per_person = 0
+    lab_program.ventilation.flow_per_area = 0
+    lab_program.ventilation.air_changes_per_hour = 6
+
+    building.properties.energy.set_all_room_2d_program_type(lab_program)
+
+    assert all(room_2d.properties.energy.program_type == lab_program
+               for room_2d in building.unique_room_2ds)
+
+    building.properties.energy.diversify()
+
+    assert all(room_2d.properties.energy.program_type != lab_program
+               for room_2d in building.unique_room_2ds)
+
+
 def test_averaged_program_type():
     """Test the averaged_program_type method."""
     pts_1 = (Point3D(0, 0, 3), Point3D(0, 10, 3), Point3D(10, 10, 3), Point3D(10, 0, 3))
