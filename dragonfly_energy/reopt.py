a8ea05d18b848ccebb5c753ed4f2706d40a3ca39
Chris Mackey <chris@ladybug.tools>
2022-02-08 16:17:46 -0500

    feat(reopt): Add support for ground-mounted PV

--- dragonfly_energy/reopt.py
+++ dragonfly_energy/reopt.py
@@ -4,8 +4,14 @@ from __future__ import division
 
 import os
 import json
+import math
 
-from honeybee.typing import float_positive, float_in_range, int_positive
+from ladybug_geometry.geometry2d.pointvector import Point2D, Vector2D
+from ladybug_geometry.geometry2d.polygon import Polygon2D
+from honeybee.typing import float_positive, float_in_range, int_positive, \
+    valid_ep_string, valid_string
+from dragonfly.projection import polygon_to_lon_lat, meters_to_long_lat_factors, \
+    origin_long_lat_from_location
 
 
 class REoptParameter(object):
@@ -334,9 +340,48 @@ class PVParameter(_SourceParameter):
     Properties:
         * max_kw
         * dollars_per_kw
+        * max_kw_ground
+        * dollars_per_kw_ground
     """
-    def __init__(self, max_kw=0, dollars_per_kw=1600):
+    def __init__(self, max_kw=0, dollars_per_kw=1600,
+                 max_kw_ground=0, dollars_per_kw_ground=2200):
         _SourceParameter.__init__(self, max_kw, dollars_per_kw)
+        self.max_kw_ground = max_kw_ground
+        self.dollars_per_kw_ground = dollars_per_kw_ground
+
+    @property
+    def max_kw_ground(self):
+        """Get or set a number for the maximum installed kilowatts of Ground PV."""
+        return self._max_kw_ground
+
+    @max_kw_ground.setter
+    def max_kw_ground(self, value):
+        self._max_kw_ground = float_positive(value, input_name='reopt max kw')
+
+    @property
+    def dollars_per_kw_ground(self):
+        """Get or set a number for the installation cost of ground PV in US dollars."""
+        return self._dollars_per_kw_ground
+
+    @dollars_per_kw_ground.setter
+    def dollars_per_kw_ground(self, value):
+        self._dollars_per_kw_ground = \
+            float_positive(value, input_name='reopt dollars per kw')
+
+    def apply_to_dict(self, base_dict):
+        """Apply this object's properties to an object of REopt schema."""
+        if isinstance(base_dict, dict):
+            base_dict['max_kw'] = self.max_kw
+            base_dict['installed_cost_us_dollars_per_kw'] = self.dollars_per_kw
+        else:
+            base_dict[0]['max_kw'] = self.max_kw
+            base_dict[0]['installed_cost_us_dollars_per_kw'] = self.dollars_per_kw
+            base_dict[1]['max_kw'] = self.max_kw_ground
+            base_dict[1]['installed_cost_us_dollars_per_kw'] = self.dollars_per_kw_ground
+
+    def __copy__(self):
+        return PVParameter(self.max_kw, self.dollars_per_kw,
+                           self.max_kw_ground, self.dollars_per_kw_ground)
 
     def ToString(self):
         """Overwrite .NET ToString."""
@@ -388,3 +433,201 @@ class GeneratorParameter(_SourceParameter):
 
     def __repr__(self):
         return 'REopt GeneratorParameter: {} kW'.format(self.max_kw)
+
+
+class GroundMountPV(object):
+    """Represents a ground-mounted photovoltaic system in REopt.
+
+    Args:
+        identifier: Text string for a unique Photovoltaic ID. Must contain only
+            characters that are acceptable in REopt. This will be used to
+            identify the object across the exported geoJSON and REopt files.
+        geometry: A Polygon2D representing the geometry of the PV field.
+        building_identifier: An optional identifier of a dragonfly Building with
+            which the photovoltaic system is associated. If None, the PV system
+            will be assumed to be a community PV field that isn't associated with
+            a particular building meter. (Default: None).
+
+    Properties:
+        * identifier
+        * display_name
+        * geometry
+        * building_identifier
+    """
+    __slots__ = ('_identifier', '_display_name', '_geometry', '_building_identifier')
+
+    def __init__(self, identifier, geometry, building_identifier=None):
+        """Initialize GroundMountPV."""
+        self.identifier = identifier
+        self._display_name = None
+        assert isinstance(geometry, Polygon2D), 'Expected ladybug_geometry ' \
+            'Polygon2D for GroundMountPV geometry. Got {}'.format(type(geometry))
+        self._geometry = geometry
+        self.building_identifier = building_identifier
+
+    @classmethod
+    def from_dict(cls, data):
+        """Initialize an GroundMountPV from a dictionary.
+
+        Args:
+            data: A dictionary representation of an GroundMountPV object.
+        """
+        # check the type of dictionary
+        assert data['type'] == 'GroundMountPV', 'Expected GroundMountPV ' \
+            'dictionary. Got {}.'.format(data['type'])
+        geo = Polygon2D.from_dict(data['geometry'])
+        pv_obj = cls(data['identifier'], geo)
+        if 'display_name' in data and data['display_name'] is not None:
+            pv_obj.display_name = data['display_name']
+        if 'building_identifier' in data and data['building_identifier'] is not None:
+            pv_obj.building_identifier = data['building_identifier']
+        return pv_obj
+
+    @property
+    def identifier(self):
+        """Get or set the text string for unique object identifier."""
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier):
+        self._identifier = valid_ep_string(identifier, 'identifier')
+
+    @property
+    def display_name(self):
+        """Get or set a string for the object name without any character restrictions.
+
+        If not set, this will be equal to the identifier.
+        """
+        if self._display_name is None:
+            return self._identifier
+        return self._display_name
+
+    @display_name.setter
+    def display_name(self, value):
+        try:
+            self._display_name = str(value)
+        except UnicodeEncodeError:  # Python 2 machine lacking the character set
+            self._display_name = value  # keep it as unicode
+
+    @property
+    def building_identifier(self):
+        """Get or set the text string for a Building associated with the PV field."""
+        return self._building_identifier
+
+    @building_identifier.setter
+    def building_identifier(self, identifier):
+        if identifier is not None:
+            identifier = valid_string(identifier, 'building identifier')
+        self._building_identifier = identifier
+
+    @property
+    def geometry(self):
+        """Get a Polygon2D representing the photovoltaic field."""
+        return self._geometry
+
+    def move(self, moving_vec):
+        """Move this object along a vector.
+
+        Args:
+            moving_vec: A ladybug_geometry Vector3D with the direction and distance
+                to move the object.
+        """
+        self._geometry = self._geometry.move(Vector2D(moving_vec.x, moving_vec.y))
+
+    def rotate_xy(self, angle, origin):
+        """Rotate this object counterclockwise in the XY plane by a certain angle.
+
+        Args:
+            angle: An angle in degrees.
+            origin: A ladybug_geometry Point3D for the origin around which the
+                object will be rotated.
+        """
+        self._geometry = self._geometry.rotate(
+            math.radians(angle), Point2D(origin.x, origin.y))
+
+    def reflect(self, plane):
+        """Reflect this object across a plane.
+
+        Args:
+            plane: A ladybug_geometry Plane across which the object will be reflected.
+        """
+        assert plane.n.z == 0, \
+            'Plane normal must be in XY plane to use it on dragonfly object reflect.'
+        norm = Vector2D(plane.n.x, plane.n.y)
+        origin = Point2D(plane.o.x, plane.o.y)
+        self._geometry = self._geometry.reflect(norm, origin)
+
+    def scale(self, factor, origin=None):
+        """Scale this object by a factor from an origin point.
+
+        Args:
+            factor: A number representing how much the object should be scaled.
+            origin: A ladybug_geometry Point3D representing the origin from which
+                to scale. If None, it will be scaled from the World origin (0, 0, 0).
+        """
+        ori = Point2D(origin.x, origin.y) if origin is not None else None
+        self._geometry = self._geometry.scale(factor, ori)
+
+    def to_dict(self):
+        """GroundMountPV dictionary representation."""
+        base = {'type': 'GroundMountPV'}
+        base['identifier'] = self.identifier
+        base['geometry'] = self.geometry.to_dict()
+        if self._display_name is not None:
+            base['display_name'] = self.display_name
+        if self._building_identifier is not None:
+            base['building_identifier'] = self.building_identifier
+        return base
+
+    def to_geojson_dict(self, location, point):
+        """Get GroundMountPV dictionary as it appears in an URBANopt geoJSON.
+
+        Args:
+            location: A ladybug Location object possessing longitude and latitude data.
+            point: A ladybug_geometry Point2D for where the location object exists
+                within the space of a scene. The coordinates of this point are
+                expected to be in the units of this Model. (Default: (0, 0)).
+        """
+        # get the conversion factors over to (longitude, latitude)
+        origin_lon_lat = origin_long_lat_from_location(location, point)
+        convert_facs = meters_to_long_lat_factors(origin_lon_lat)
+
+        # create the GeoJSON dictionary
+        pts = [(pt.x, pt.y) for pt in self.geometry.vertices]
+        coords = [polygon_to_lon_lat(pts, origin_lon_lat, convert_facs)]
+        base = {
+            'type': 'Feature',
+            'properties': {
+                'id': self.identifier,
+                'geometryType': 'Rectangle',
+                'name': self.display_name,
+                'type': 'District System',
+                'footprint_area': self.geometry.area,
+                'footprint_perimeter': self.geometry.perimeter
+            },
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': coords
+            }
+        }
+        if self.building_identifier is None:
+            base['properties']['district_system_type'] = 'Community Photovoltaic'
+        else:
+            base['properties']['district_system_type'] = 'Ground Mount Photovoltaic'
+            base['properties']['associated_building_id'] = self.building_identifier
+        return base
+
+    def duplicate(self):
+        """Get a copy of this object."""
+        return self.__copy__()
+
+    def __copy__(self):
+        new_obj = GroundMountPV(self.identifier, self.geometry, self.building_identifier)
+        new_obj._display_name = self._display_name
+        return new_obj
+
+    def ToString(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return 'GroundMountPV: {}'.format(self.display_name)
