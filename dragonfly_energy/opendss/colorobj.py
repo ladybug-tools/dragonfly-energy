cf1a6bd1c84e61e4b8a85d9f701e6ab8cc89d9fd
Chris Mackey <chris@ladybug.tools>
2022-08-16 09:24:47 -0400

    fix(colorobj): Ensure ColorNetwork works with 2D geometries

--- dragonfly_energy/opendss/colorobj.py
+++ dragonfly_energy/opendss/colorobj.py
@@ -1,4 +1,6 @@
 """Module for coloring OpenDSS geometry with attributes."""
+from ladybug_geometry.geometry2d import Point2D
+from ladybug_geometry.geometry3d import Point3D
 from honeybee.colorobj import _ColorObject
 
 from .network import ElectricalNetwork
@@ -56,6 +58,36 @@ class ColorElectricalNetwork(_ColorObject):
         """A tuple of Polygon2D, Polyline2D and LineSegment2D aligned with attributes.
         """
         return self._geometries
+
+    @property
+    def min_point(self):
+        """Get a Point3D for the minimum of the box around the objects."""
+        return Point3D(self._min_point.x, self._min_point.y, 0)
+
+    @property
+    def max_point(self):
+        """Get a Point3D for the maximum of the box around the objects."""
+        return Point3D(self._max_point.x, self._max_point.y, 0)
+
+    def _calculate_min_max(self, hb_objs):
+        """Calculate maximum and minimum Point3D for a set of objects."""
+        st_rm_min, st_rm_max = hb_objs[0].geometry.min, hb_objs[0].geometry.max
+        min_pt = [st_rm_min.x, st_rm_min.y]
+        max_pt = [st_rm_max.x, st_rm_max.y]
+
+        for obj in hb_objs[1:]:
+            rm_min, rm_max = obj.geometry.min, obj.geometry.max
+            if rm_min.x < min_pt[0]:
+                min_pt[0] = rm_min.x
+            if rm_min.y < min_pt[1]:
+                min_pt[1] = rm_min.y
+            if rm_max.x > max_pt[0]:
+                max_pt[0] = rm_max.x
+            if rm_max.y > max_pt[1]:
+                max_pt[1] = rm_max.y
+
+        self._min_point = Point2D(min_pt[0], min_pt[1])
+        self._max_point = Point2D(max_pt[0], max_pt[1])
 
     def __repr__(self):
         """Color ElectricalNetwork representation."""
