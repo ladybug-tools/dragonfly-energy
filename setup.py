206976d310816b5320e801c8fa99f9e0c5b56637
Chris Mackey <chris@ladybug.tools>
2023-01-09 12:08:39 -0500

    fix(docker): Include standards-requirements.txt in docker

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-#with open('standards-requirements.txt') as f:
-#    standards_requirements = f.read().splitlines()
-standards_requirements = 'honeybee-energy-standards==2.2.0'
+with open('standards-requirements.txt') as f:
+    standards_requirements = f.read().splitlines()
 
 setuptools.setup(
     name="dragonfly-energy",
