3fbda3b407f8b12ae6286da4c263dddcf3d56fba
Chris Mackey <chris@ladybug.tools>
2024-12-06 11:44:31 -0800

    fix(translate): Refactor commands to be call-able outside the CLI

--- tests/cli_translate_test.py
+++ tests/cli_translate_test.py
@@ -1,8 +1,8 @@
 """Test cli translate module."""
 from click.testing import CliRunner
 from ladybug.futil import nukedir
-from dragonfly_energy.cli.translate import model_to_osm, model_to_idf, \
-    model_to_gbxml, model_to_sdd
+from dragonfly_energy.cli.translate import model_to_osm_cli, model_to_idf_cli, \
+    model_to_gbxml_cli, model_to_sdd_cli
 
 import os
 
@@ -13,7 +13,7 @@ def test_model_to_osm():
 
     output_df_folder = './tests/json/osm'
     output_df_model = os.path.join(output_df_folder, 'run', 'in.osm')
-    result = runner.invoke(model_to_osm, [input_df_model, '-f', output_df_folder])
+    result = runner.invoke(model_to_osm_cli, [input_df_model, '-f', output_df_folder])
     assert result.exit_code == 0
 
     assert os.path.isfile(output_df_model)
@@ -26,7 +26,7 @@ def test_model_to_idf():
 
     output_df_folder = './tests/json/'
     output_df_model = os.path.join(output_df_folder, 'OfficeBuilding.idf')
-    result = runner.invoke(model_to_idf, [input_df_model, '-f', output_df_model])
+    result = runner.invoke(model_to_idf_cli, [input_df_model, '-f', output_df_model])
     assert result.exit_code == 0
 
     assert os.path.isfile(output_df_model)
@@ -39,7 +39,7 @@ def test_model_to_gbxml():
 
     output_df_folder = './tests/json/gbxml'
     output_df_model = os.path.join(output_df_folder, 'in.xml')
-    result = runner.invoke(model_to_gbxml, [input_df_model, '-f', output_df_model])
+    result = runner.invoke(model_to_gbxml_cli, [input_df_model, '-f', output_df_model])
     assert result.exit_code == 0
 
     assert os.path.isfile(output_df_model)
@@ -52,7 +52,7 @@ def test_model_to_sdd():
 
     output_df_folder = './tests/json/sdd'
     output_df_model = os.path.join(output_df_folder, 'in.xml')
-    result = runner.invoke(model_to_sdd, [input_df_model, '-f', output_df_model])
+    result = runner.invoke(model_to_sdd_cli, [input_df_model, '-f', output_df_model])
     assert result.exit_code == 0
 
     assert os.path.isfile(output_df_model)
